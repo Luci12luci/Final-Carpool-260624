15b383ca5232844ec2fbc3a54fad1971adad127a
Luci12 <mihalikova.lucia@yahoo.com>
2024-06-26 20:05:47 +0200

    Initial

--- app.py
+++ app.py
@@ -31,5 +31,18 @@ def welcome(username):
     return render_template('welcome.html', username=username)
 
 
+@app.route('/create_ad', methods=['GET', 'POST'])
+def create_ad():
+    if request.method == 'POST':
+        # Spracovanie údajov z formulára (trasa, čas, podmienky)
+        # Uložte inzerát do databázy
+        return redirect(url_for('success'))
+    return render_template('create_ad.html')
+
+@app.route('/success')
+def success():
+    return 'Inzerát bol úspešne vytvorený!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
